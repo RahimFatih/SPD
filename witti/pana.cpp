ddc2dea177bc74b0e69d325e4f2da5970b3e4045
RahimFatih <65657905+RahimFatih@users.noreply.github.com>
2021-04-09 09:33:58 +0200

    a

--- witti/pana.cpp
+++ witti/pana.cpp
@@ -17,18 +17,32 @@ int main()
     for (int i = 0; i < n; i++)
         cout << P[i] << " " << W[i] << " " << D[i] << endl;
     //
-    int N = 1 << n, *F = new int[N];
-    F[0] = 0;
-    for (int set = 1; set < N; set++)
+    //Dane załadwoane
+    //
+    int N = 1 << n, *F = new int[N];//Stwórz tablicę w Intów, w której przechowywane będą informajce o najlepszych wynikach dla konkretnych konfiguracji
+    //
+    //
+    F[0] = 0;//ustaw wartosc zerowej permutajci jako 0
+    //
+    //
+    //
+    for (int set = 1; set < N; set++)//powtarzaj dla wszystkich możliwych permutacji SET jest maską bitową
     {
         int c = 0;
-        for (int i = 0, b = 1; i < n; i++)
+        for (int i = 0,b=1; i < n; i++,b*=2)//powtarzaj dla wszystkich procesow
             if (set & b)
                 c += P[i];
+
         F[set] = 9999999;
         for (int k = 0, b = 1; k < n; k++, b *= 2)
             if (set & b)
             {
+                //cout<<"Fset: "<<F[set]<<endl;
+                //cout<<"Fset-b: "<<F[set-b]<<endl;
+                //cout<<"Wk: "<< W[k]<<endl;
+                //cout<<"c: "<<c<<endl;
+                //cout<<"Dk: "<<D[k]<<endl;
+                //cin.get();
 
                 if (F[set]>F[set - b] + W[k] * max(c - D[k], 0))
                 {
@@ -36,7 +50,7 @@ int main()
                 }
             }
     }
-    cout << F[N - 1] << endl;
+    //cout << F[N - 1] << endl;
     delete[] F;
     cin.get();
     return 0;
---
+++ witti/program.py
@@ -0,0 +1,66 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+import numpy
+import re
+p = []
+w = []
+d = []
+with open("dataPY.txt","r") as data:
+    for n in range(10):
+        s=(data.readline()).split()
+        p.append(int(s[0]))
+        w.append(int(s[1]))
+        d.append(int(s[2]))
+
+
+# %%
+n=len(p)
+N=1<<n
+F=[0]
+ListaL=[[]]
+print(n)
+print(N)
+print(ListaL[0])
+
+
+# %%
+for x in range(3):
+    print(x)
+
+
+# %%
+for set in range(1,N):
+    sumaP=0
+    b=1
+    c=0
+    #####zliczanie sumy####
+    for j in range(n):
+        if (set&b):
+            c=c+p[j]
+        b*=2
+    ####DZIALA!!!!!###
+    F.append(99999999)
+    ListaL.append([])
+    b=1
+    for j in range(n):
+        if (set&b):
+            if (F[set]>F[set - b] + w[j] * max(c - d[j], 0)):
+                pomocniczy=ListaL[set-b].copy()
+                pomocniczy.append(j+1)
+                ListaL[set]=pomocniczy.copy()
+                F[set]=F[set - b] + w[j] * max(c - d[j], 0)
+        b=b*2
+
+
+# %%
+print(F[-1])
+
+
+# %%
+print(ListaL[-1])
+
+
+
+
+
